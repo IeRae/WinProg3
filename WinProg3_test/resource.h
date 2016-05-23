57aa96242ccc51be783edbeacca3058ea6e05196
인웅 정 <jungiw7@naver.com>
2016-05-23 22:10:20 +0900

    5/23, 정인웅, 비트맵 삽입

--- WinProg3_test/resource.h
+++ WinProg3_test/resource.h
@@ -11,13 +11,28 @@
 #define IDB_BITMAP2                     315
 #define IDB_OUTPUT_TRUE                 315
 #define IDB_OUTPUT_FALSE                316
+#define IDB_AND                         317
+#define IDB_OR                          318
+#define IDB_BITMAP1                     319
+#define IDB_NAND                        319
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_AND                          32777
+#define ID_OR                           32778
+#define ID_NOT                          32779
+#define ID_NAND                         32780
+#define ID_NOR                          32781
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        317
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        320
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
