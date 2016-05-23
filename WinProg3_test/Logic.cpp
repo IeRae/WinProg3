3b6f8a37b2068b59b5c107649709efe9ef4f85ad
인웅 정 <jungiw7@naver.com>
2016-05-24 00:09:53 +0900

    5/24, 정인웅 , Logic클래스 내 논리연산자 정의

--- WinProg3_test/Logic.cpp
+++ WinProg3_test/Logic.cpp
@@ -11,6 +11,13 @@ Logic::~Logic()
 {
 }
 
+
+bool Logic::And_Operator(bool A1, bool A2) { return (A1 & A2); }
+
+bool Logic::Or_Operator(bool A1, bool A2) { return (A1 | A2); }
+
+bool Logic::Nand_Operator(bool A1, bool A2) { return !(A1 & A2); }
+
 bool Logic::Not_Operator(bool A) { return !A; }
 
 bool Logic::Nor_Operator(bool A1, bool A2) { return !(A1|A2); }
