003779b9be15e611da8ee0e252992f3ee6283d1a
IeRae <duddml314@gmail.com>
2016-05-24 23:47:30 +0900

    5/24, 윤이래, 논리 회로 선에 관한 구조체 정의

--- WinProg3_test/WinProg3_testView.h
+++ WinProg3_test/WinProg3_testView.h
@@ -9,12 +9,30 @@ struct Gate {
 	int GateId;
 	int x;
 	int y;
+	
 	//입력값 정보
 	bool inputArray[MAX];
+	int inputArrayIndex;
+	
 	//출력값 정보
 	bool outputArray[MAX];
+	int outputArrayIndex;
 	Gate() {}
 	Gate(int a, int b, int c) { GateId = a; x = b; y = c; }
+};
+
+struct Line {
+	//시작 bool value 변수
+	bool startBoolValue;
+
+	//시작 Gate index
+	int startGateIndex;
+	
+	//종료 bool value 변수
+	bool endBoolValue;
+	
+	//종료 Gate index
+	int endGateIndex;
 };
 
 class CWinProg3_testView : public CView
