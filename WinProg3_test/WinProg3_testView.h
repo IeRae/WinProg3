6c1d2c7b3c3370f7b620734a9618d091e7179981
IeRae <duddml314@gmail.com>
2016-05-25 00:05:57 +0900

    5/24, 윤이래, Gate와 Line 에 관한 구조체 분리 컴파일

--- WinProg3_test/WinProg3_testView.h
+++ WinProg3_test/WinProg3_testView.h
@@ -1,42 +1,9 @@
 
 // WinProg3_testView.h : CWinProg3_testView 클래스의 인터페이스
 
-#define MAX 1024
 
 #pragma once
-struct Gate {
-	//비트맵 정보
-	int GateId;
-	int x;
-	int y;
-	
-	//입력값 정보
-	bool inputArray[MAX];
-	int inputArrayIndex;
-	
-	//출력값 정보
-	bool outputArray[MAX];
-	int outputArrayIndex;
-	Gate() {}
-	Gate(int a, int b, int c) { GateId = a; x = b; y = c; }
-};
 
-struct Line {
-	//시작 bool value 변수
-	bool startBoolValue;
-
-	//시작 Gate index
-	int startGateIndex;
-	
-	//종료 bool value 변수
-	bool endBoolValue;
-	
-	//종료 Gate index
-	int endGateIndex;
-
-	Line() {}
-	Line(bool start, int start_in, bool end, int end_in) { startBoolValue = start; startGateIndex = start_in; endBoolValue = end; endGateIndex = end_in; }
-};
 
 class CWinProg3_testView : public CView
 {
